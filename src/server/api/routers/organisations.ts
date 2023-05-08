5f2ab467b16eceaed25b8e3096c3e4cc94395f71
Ionut Panduru <panduru.ionut@hotmail.com>
2023-05-08 18:47:00 +0300

    feat: add organisations router

---
+++ src/server/api/routers/organisations.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const oranisationsRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    return ctx.prisma.organisation.findMany();
+  }),
+});
