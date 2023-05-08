b98bd105473f82578a369d08da6cb5d26c2735cb
Ionut Panduru <panduru.ionut@hotmail.com>
2023-05-08 12:33:48 +0300

    feat: add login functionality with clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,34 @@
 import { type AppType } from "next/app";
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+
+const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]"];
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  const { pathname } = useRouter();
+
+  const isPublicPage = publicPages.includes(pathname);
+  return (
+    <ClerkProvider {...pageProps}>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);
