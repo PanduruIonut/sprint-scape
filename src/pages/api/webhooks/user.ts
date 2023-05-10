5d0766a21afd44b389c057aa7dc75b092386b131
Ionut Panduru <panduru.ionut@hotmail.com>
2023-05-10 16:39:08 +0300

    chore: display data from webhook event

--- src/pages/api/webhooks/user.ts
+++ src/pages/api/webhooks/user.ts
@@ -2,9 +2,10 @@
 import type { WebhookEvent } from '@clerk/clerk-sdk-node'
 import { type NextApiHandler } from 'next'
 
-const handler: NextApiHandler = (req) => {
+const handler: NextApiHandler = (req, res) => {
     const evt = req.body.evt as WebhookEvent
     console.log(evt)
+    res.status(200).json(evt.data)
 }
 
 export default handler
