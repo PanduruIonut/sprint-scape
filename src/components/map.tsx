88357cc88b209fa811155e6a3ff98752604ef9bd
Ionut Panduru <panduru.ionut@hotmail.com>
2023-05-24 18:28:27 +0300

    feat: Add a marker to current user location

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -14,11 +14,41 @@ export function Map({
     useEffect(() => {
         if (!window) return
         if (!ref.current) return
-        new window.google.maps.Map(ref.current, {
+        const map = new window.google.maps.Map(ref.current, {
             center,
             zoom,
         });
+        const infoWindow = new google.maps.InfoWindow();
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position: GeolocationPosition) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+
+                    infoWindow.setPosition(pos);
+                    map.setCenter(pos);
+                    map.setZoom(13);
+                    new google.maps.Marker({
+                        position: pos,
+                        map,
+                        label: { text: "You're here!", color: "black", fontSize: "16px" }
+                    });
+                },
+                () => {
+                    handleLocationError(true, infoWindow);
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow);
+        }
     });
 
-    return <div ref={ref} id="map" style={{ width: "650px", height: "650px" }} />;
+    return <div ref={ref} id="map" style={{ width: "1250px", height: "550px" }} />;
 }
+function handleLocationError(_arg0: boolean, _infoWindow: google.maps.InfoWindow) {
+    throw new Error("Function not implemented.");
+}
+
