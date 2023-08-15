0a3539285dbf7b481d840be5b704ba951fe59091
Ionut Panduru <panduru.ionut@hotmail.com>
2023-08-15 14:41:23 +0300

    Fix: remove unused code

--- src/components/venue/venues.tsx
+++ src/components/venue/venues.tsx
@@ -1,12 +1,9 @@
 import { api } from "@/utils/api";
-import { useOrganization } from "@clerk/nextjs";
 import Masonry from "../templates/masonry";
 
 export default function Venues({ facilityId }: { facilityId: string }) {
 
-    const { organization } = useOrganization();
 
-    if (!organization) return null
     const venues = api.venue.getAllByFacilityId.useQuery({ facilityId: facilityId, pictures: true });
     return (
         <>
